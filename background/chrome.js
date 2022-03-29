f3328abee9dc57a560d50395e37bf01107508380
Jochen Weisser <jochen@wei3er.com>
2022-03-29 07:16:12 +0200

    bugfix: messaging

--- background/chrome.js
+++ background/chrome.js
@@ -1,4 +1,27 @@
-
 function parseChrome(raw) {
-    throw "not yet implemented!";
+    var items = [];
+    parseChromeNode(items, [], JSON.parse(raw));
+    return items;
+}
+
+function parseChromeNode(items, path, thisNode) {
+    if(true) throw "not yet implemented!"
+    var p = path;
+    if(thisNode.children) {
+        if(!thisNode.root && thisNode.typeCode == 2) {
+            p = [...p, thisNode.title];
+        }
+        for(child of thisNode.children) {
+            parseChromeNode(items, p, child)
+        }
+    } else {
+        if(thisNode.typeCode == 1 && thisNode.uri) {
+            items.push({
+                path: p,
+                title: thisNode.title,
+                index: thisNode.index,
+                url: thisNode.uri,
+            });
+        }
+    }
 }
